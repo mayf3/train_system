3f26fb9502a8370126b571abc7c9d49b7cf7e375
mayf3 <daretobeyourself.m.y.f@gmail.com>
2015-03-11 20:44:22 +0800

    edit index controller

--- controllers/index.go
+++ controllers/index.go
@@ -9,5 +9,10 @@ type IndexController struct {
 }
 
 func (c *IndexController) Get() {
+    info := []map[string]string{}
+    //info = models.GetTables();
+    info = append(info, map[string]string{"date":"1", "title":"first", "source":"fs"})
+    info = append(info, map[string]string{"date":"2", "title":"second", "source":"ss"})
+    c.Data["Map"] = info;
 	c.TplNames = "index.tpl"
 }
