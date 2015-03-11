7c61f6068d66ff92dabbab3fc1c4565bc4a0f9cf
mayf3 <daretobeyourself.m.y.f@gmail.com>
2015-03-11 16:39:51 +0800

    make sure that view can work

--- routers/router.go
+++ routers/router.go
@@ -6,5 +6,10 @@ import (
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+    beego.Router("/", &controllers.IndexController{})
+    beego.Router("/create_imformation", &controllers.CreateImformationController{})
+    beego.Router("/create_table", &controllers.CreateTableController{})
+    beego.Router("/edit_imformation", &controllers.EditImformationController{})
+    beego.Router("/edit_table", &controllers.EditTableController{})
+    beego.Router("/generate_table", &controllers.GenerateTableController{})
 }
